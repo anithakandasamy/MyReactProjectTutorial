cc6029301238973b2129949a50fa5ea43ef95a3f
anithakandasamy <anitha.kandasamy@shell.com>
2023-05-17 19:32:28 +0530

    cleared warning

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from "./components/body/Body";
 import PropsCardParent from "./components/propsCardParent/PropsCardParent";
 import Categories from "./components/categories/Categories";
 import StateCard from "./components/stateCard/StateCard";
-import CardModalFunction from "./components/cardModalFunction/CardModalFunction"
+import CardModalFunction from "./components/cardModalFunction/CardModalFunction";
 import StateDemo from "./components/stateDemo/StateDemo";
 import CardBootstrapModal from "./components/cardBootstrapModal/CardBootstrapModal";
 import Employee from "./components/employee/Employee";
@@ -16,15 +16,15 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
-      {/*  <Categories></Categories>
-     <Carousel></Carousel>
-      <Employee></Employee>*/}
-      <hr/>
+      <Categories></Categories>
+      <Carousel></Carousel>
+      <Employee></Employee>
+      <hr />
       <Search></Search>
       <Card1></Card1>
       <PropsCardParent></PropsCardParent>
       <StateCard></StateCard>
-      <CardModalFunction></CardModalFunction> 
+      <CardModalFunction></CardModalFunction>
       <CardBootstrapModal></CardBootstrapModal>
       <StateDemo></StateDemo>
       <Body></Body>
